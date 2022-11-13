2e510b5ca619f9992507f37cc7eba366dcf6204e
Calvin Bijsmans <calvinbijsmans@hotmail.com>
2022-11-13 20:24:57 +0100

    add actix web

--- adapters/src/lib.rs
+++ adapters/src/lib.rs
@@ -1,10 +1,1 @@
-#[cfg(test)]
-
-mod tests {
-    use use_cases::use_case::create_generator::CreateGeneratorUseCase;
-
-    #[test]
-    fn it_works() {
-        let test = CreateGeneratorUseCase;
-    }
-}
+pub mod rest;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
 fn main() {
+    use adapters::rest::usecases;
+    usecases::main();
     println!("Hello, world!");
 }
